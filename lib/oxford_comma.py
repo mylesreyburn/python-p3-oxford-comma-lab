b353860e80270450811e4085a62e0474ecac78fe
Myles Reyburn <mylesreyburn@gmail.com>
2023-03-19 17:12:44 -0600

    done

--- lib/oxford_comma.py
+++ lib/oxford_comma.py
@@ -1,2 +1,14 @@
 def oxford_comma(items):
-    return None
+    if len(items) == 1:
+        return items[0]
+    elif len(items) == 2:
+        return " and ".join(items)
+    else:  
+        items[-1] = f"and {items[-1]}"
+
+        joined_items = ", ".join(items)
+        return joined_items
+
+print("one element: " + oxford_comma(["Malians"]))
+
+print("four elements: " + oxford_comma(["Teutons", "Aztecs", "Hindustani", "Spanish"]))
